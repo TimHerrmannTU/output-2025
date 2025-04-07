f45bd0085addeaaee36b29ba921b515bf9a764de
Tim Herrmann <platinum2dollar@gmail.com>
2025-04-07 13:40:25 +0200

    Added the RSA-Riddle

---
+++ includes/geocaching/rsa.php
@@ -0,0 +1,36 @@
+<div class="light-bg">
+    <div class="wrapper col gap-3">
+        <h3 class="pt-3 pb-1">RSA-VERSCHLÜSELUNG</h3>
+        <div class="col gap-1">
+            <p>
+                Stell dir vor, du hast ein Vorhängeschloss, das jeder zusperren kann, aber nur du besitzt den Schlüssel zum Aufsperren. 
+                Genauso funktioniert das RSA-Verfahren: Es nutzt zwei Schlüssel, einen öffentlichen und einen privaten.
+            </p>
+            <ul class="ml-2">
+                <li>Mit dem öffentlichen Schlüssel (dem „Vorhängeschloss“) kann jeder deine Nachricht verschlüsseln, damit sie geheim bleibt.</li>
+                <li>Nur mit deinem privaten Schlüssel (dem „passenden Schlüssel zum Schloss“) kannst du sie wieder öffnen und lesen. </li>
+            </ul>
+            <p>
+                Dieses Prinzip nennt man „asymmetrische Verschlüsselung“. Es kommt zum Beispiel beim sicheren Surfen im Internet, 
+                bei E-Mails und sogar beim Online-Banking zum Einsatz – überall dort, wo vertrauliche Daten geschützt werden müssen.
+            </p>
+        </div>
+        <div class="col gap-1">
+            <h4>Schlüsselgenerierung</h4>
+            <ol type="1" class="ml-2">
+                <li>Zuerst werden zwei (große) Primzahlen gewählt p und q</li>
+                <li>Dann wird das RSA Modul n berechnet als Produkt dieser beiden Zahlen, dieses dient der Ver- und Entschlüsselung und ist somit Teil des privaten und des öffentlichen Schlüssels.</li>
+                <li>Als nächstes berechnet man die Eulersche Phi-Funktion Z von n als Produkt von p-1 und q-1.</li>
+                <li>Der zweite Teil des öffentlichen Schlüssels e und der zweite Teil des privaten Schlüssels d werden dann mit Hilfe von Z berechnet.</li>
+            </ol>
+        </div>
+        <div class="col gap-1">
+            <h4>RÄTSEL</h4>
+            <p>
+                Sei p=4 und q=2.<br>
+                Um die Koordinaten des nächsten Ziels zu erhalten müssen in den untenstehenden Koordinaten n und z durch ihre jeweiligen Werte ersetzt werden
+            </p>
+            <p><b>N51°01.465' E01<span style="color:red">z</span>°43.30<span style="color:red">n</span></b></p>
+        </div>
+    </div>
+</div>
