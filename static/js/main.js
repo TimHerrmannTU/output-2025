f3fd13c2866c3c8ba522ed777ca4e052e6ece59a
Tim Herrmann <platinum2dollar@gmail.com>
2025-04-09 21:03:55 +0200

    Finished styling input boxes & textareas

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,28 @@
 jQuery(document).ready(function($) {
+    // scroll event for the magenta cube inside the banner
     var last_y = $(document).scrollTop();
-    var offset_y = parseInt($("#banner:not(.slim) .cube-unfolded").css("top").replace("px", ""));
-    $(window).on( "scroll", function() {
-        if (offset_y < 305) {
-            var new_y = $(this).scrollTop();
-            if (last_y < new_y) {
-                offset_y += (new_y - last_y)/2
-                $("#banner .cube-unfolded").css("top", (offset_y).toString()+"px")
+    try {
+        var offset_y = parseInt($("#banner:not(.slim) .cube-unfolded").css("top").replace("px", ""));
+        $(window).on( "scroll", function() {
+            if (offset_y < 305) {
+                var new_y = $(this).scrollTop();
+                if (last_y < new_y) {
+                    offset_y += (new_y - last_y)/2
+                    $("#banner .cube-unfolded").css("top", (offset_y).toString()+"px")
+                }
+                last_y = $(this).scrollTop()
             }
-            last_y = $(this).scrollTop()
+        })
+    }
+    catch(e) {}
+    // labeled input event (keeps the label above if there is content inside the input)
+    $(".labeled-input input").on("keyup", function() {
+        var parent = $(this).closest(".labeled-input")
+        if ($(this).val() != "") {
+            $(parent).addClass("has-content")
+        }
+        else {
+            $(parent).removeClass("has-content")
         }
     })
 })
