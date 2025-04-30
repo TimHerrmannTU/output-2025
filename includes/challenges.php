3d09c7c12efc73f93769cfaa673cf4e6de458a82
Tim Herrmann <platinum2dollar@gmail.com>
2025-04-30 11:08:25 +0200

    Moved challenge images to wp

--- includes/challenges.php
+++ includes/challenges.php
@@ -2,17 +2,17 @@
     <div class="wrapper cards row gap-3">
         <div class="card gc col gap-1">
             <h3>GEOCACHING</h3>
-            <img src="<?= get_template_directory_uri(); ?>/static/img/geoCaching.png"/>
+            <img src="https://output-dd.de/wp-content/uploads/2025/04/challenge_geocaching_huhnchen.png"/>
             <a href="<?= get_site_url(); ?>/geo-caching">Zur Anleitung ...</a>
         </div>
         <div class="card cc col gap-1">
             <h3>CREATIVE CHALLENGE</h3>
-            <img src="<?= get_template_directory_uri(); ?>/static/img/creativeChallenge.png"/>
+            <img src="https://output-dd.de/wp-content/uploads/2025/04/challenge_creative_maus.png"/>
             <a href="<?= get_site_url(); ?>/creative-challenge">Zur Teilnahme ...</a>
         </div>
         <div class="card lp col gap-1">
             <h3>LAN - PARTY</h3>
-            <img src="<?= get_template_directory_uri(); ?>/static/img/lanParty.png"/>
+            <img src="https://output-dd.de/wp-content/uploads/2025/04/challenge_lan_fuchs.png"/>
             <a href="<?= get_site_url(); ?>/lan-party">Zur Anmeldung ...</a>
         </div>
     </div>
