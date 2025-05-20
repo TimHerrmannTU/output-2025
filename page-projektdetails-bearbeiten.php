48e5fafb6b062ff9b9f11ef4517f78da856a7472
Charles Krüger <charles.krueger.social@gmail.com>
2025-05-20 15:56:47 +0200

    fix typo

--- page-projektdetails-bearbeiten.php
+++ page-projektdetails-bearbeiten.php
@@ -9,12 +9,14 @@ if (!is_user_logged_in()) {
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body id="projekt-einreichen" <?php body_class() ?>>
     <?php
     $main_headline = "PROJEKT BEARBEITEN";
@@ -27,12 +29,13 @@ if (!is_user_logged_in()) {
 
     <div class="light-bg">
         <div class="wrapper col gap-2">
-            
-            <form id="project-register-form" class="grid conditional" action="<?= admin_url('admin-post.php') ?>" method="POST" enctype="multipart/form-data">
+
+            <form id="project-register-form" class="grid conditional" action="<?= admin_url('admin-post.php') ?>"
+                method="POST" enctype="multipart/form-data">
                 <input type="hidden" name="action" value="submit_project_post">
 
                 <div class="labeled-input full">
-                    <label for="details-name">Title *</label>
+                    <label for="details-name">Titel *</label>
                     <input name="details-name" type="text" value="<?= the_title() ?>" required>
                 </div>
 
@@ -41,13 +44,18 @@ if (!is_user_logged_in()) {
                         <div class="iconify" data-icon="mdi-triangle-down"></div>
                     </div>
                     <select id="mode" name="category" required>
-                        <option class="template" <?= ($pro_type=="default") ? 'selected="selected"' : "" ?> value="default">Projektkategorie auswählen *</option>
-                        <option class="template" <?= ($pro_type=="demo"   ) ? 'selected="selected"' : "" ?> value="demo"   >Projektdemo</option>
-                        <option class="template" <?= ($pro_type=="poster" ) ? 'selected="selected"' : "" ?> value="poster" >Projektposter</option>
-                        <option class="template" <?= ($pro_type=="vortrag") ? 'selected="selected"' : "" ?> value="vortrag">Fachvortrag</option>
+                        <option class="template" <?= ($pro_type=="default") ? 'selected="selected"' : "" ?>
+                            value="default">Projektkategorie auswählen *</option>
+                        <option class="template" <?= ($pro_type=="demo"   ) ? 'selected="selected"' : "" ?>
+                            value="demo">Projektdemo</option>
+                        <option class="template" <?= ($pro_type=="poster" ) ? 'selected="selected"' : "" ?>
+                            value="poster">Projektposter</option>
+                        <option class="template" <?= ($pro_type=="vortrag") ? 'selected="selected"' : "" ?>
+                            value="vortrag">Fachvortrag</option>
                     </select>
                 </div>
-                <div class="file-upload col gap-1 c2" dd-function="file-upload-trigger" key="1" style="grid-row: span 3; aspect-ratio: 4/3; overflow: hidden;">
+                <div class="file-upload col gap-1 c2" dd-function="file-upload-trigger" key="1"
+                    style="grid-row: span 3; aspect-ratio: 4/3; overflow: hidden;">
                     <div class="icon">
                         <span class="iconify" data-icon="mdi-cloud-upload-outline">
                     </div>
@@ -57,19 +65,23 @@ if (!is_user_logged_in()) {
                     </label>
                     <img class="preview" src="">
                 </div>
-                <input name="details-thumbnail" type="file" accept="image/*" dd-function="file-upload-input" key="1" required>
+                <input name="details-thumbnail" type="file" accept="image/*" dd-function="file-upload-input" key="1"
+                    required>
                 <div class="labeled-input c1">
                     <label for="details-presenter">Präsentator *</label>
-                    <input name="details-presenter" type="text" value="<?= get_field("project-details-presenter") ?>" required>
+                    <input name="details-presenter" type="text" value="<?= get_field("project-details-presenter") ?>"
+                        required>
                 </div>
                 <div class="labeled-input c1">
                     <label for="details-description">Beschreibung des Projektes (max. 2000 Zeichen) *</label>
-                    <textarea name="details-description" required><?= get_field("project-details-description") ?></textarea>
+                    <textarea name="details-description"
+                        required><?= get_field("project-details-description") ?></textarea>
                 </div>
 
                 <div class="labeled-input full">
                     <label for="details-participants">Weitere Beteiligte</label>
-                    <input name="details-participants" type="text" value="<?= get_field("project-details-participants") ?>">
+                    <input name="details-participants" type="text"
+                        value="<?= get_field("project-details-participants") ?>">
                 </div>
 
                 <div class="labeled-input c1">
@@ -78,37 +90,44 @@ if (!is_user_logged_in()) {
                 </div>
                 <div class="labeled-input c2">
                     <label for="details-institution">Studiengang / Lehrstuhl / Firma</label>
-                    <input name="details-institution" type="text" value="<?= get_field("project-details-institution") ?>">
+                    <input name="details-institution" type="text"
+                        value="<?= get_field("project-details-institution") ?>">
                 </div>
 
                 <div class="labeled-input c1">
                     <label for="intern-contact-person">Ansprechpartner *</label>
-                    <input name="intern-contact-person" type="text" value="<?= get_field("project-intern-contact-person") ?>" required>
+                    <input name="intern-contact-person" type="text"
+                        value="<?= get_field("project-intern-contact-person") ?>" required>
                 </div>
                 <div class="labeled-input c2">
                     <label for="intern-contact-person-email">E-Mail *</label>
-                    <input name="intern-contact-person-email" type="text" value="<?= get_field("intern-contact-person-email") ?>" required>
+                    <input name="intern-contact-person-email" type="text"
+                        value="<?= get_field("intern-contact-person-email") ?>" required>
                 </div>
                 <div class="pt-1 full"></div>
                 <!-- START: CONDITIONAL SECTION -->
                 <!-- if demo -->
                 <p class="fat full" dd-mode="demo">Ich benötige für meinen Projektstand:</p>
                 <label class="labeled-checkbox transparent c1" dd-mode="demo">
-                    <input type="checkbox" name="intern-furniture" <?= (get_field("project-intern-furniture")==1) ? 'checked="checked"' : "" ?>>
+                    <input type="checkbox" name="intern-furniture"
+                        <?= (get_field("project-intern-furniture")==1) ? 'checked="checked"' : "" ?>>
                     <span>Tisch (inkl. Husse) und Stühle</span>
                 </label>
                 <label class="labeled-checkbox transparent c2" dd-mode="demo">
-                    <input type="checkbox" name="intern-poster-stand" <?= (get_field("project-intern-poster-stand")==1) ? 'checked="checked"' : "" ?>>
+                    <input type="checkbox" name="intern-poster-stand"
+                        <?= (get_field("project-intern-poster-stand")==1) ? 'checked="checked"' : "" ?>>
                     <span>Posteraufsteller (für Poster bis max. DIN A1 Hochformat)</span>
                 </label>
                 <div class="labeled-input full" dd-mode="demo">
                     <label for="intern-comment">Sonstige Wünsche oder Kommentare für dein Projektstand</label>
-                    <textarea name="intern-comment" style="min-height: 10rem"><?= get_field("project-intern-comment") ?></textarea>
+                    <textarea name="intern-comment"
+                        style="min-height: 10rem"><?= get_field("project-intern-comment") ?></textarea>
                 </div>
                 <!-- if vortrag -->
                 <div class="labeled-input c1" dd-mode="vortrag">
                     <label for="intern-comment">Sonstige Kommentare zu deinem Fachvortrag</label>
-                    <textarea name="intern-comment" style="min-height: 10rem"><?= get_field("project-intern-comment") ?></textarea>
+                    <textarea name="intern-comment"
+                        style="min-height: 10rem"><?= get_field("project-intern-comment") ?></textarea>
                 </div>
                 <div class="row gap-1 c2" dd-function="file-upload-trigger" key="2" dd-mode="vortrag">
                     <div class="icon">
@@ -128,11 +147,13 @@ if (!is_user_logged_in()) {
                         Lade hier dein Poster als PDF hoch (Drag and Drop oder klicke hier)
                     </label>
                 </div>
-                <input name="details-upload" type="file" accept=".pdf" dd-function="file-upload-input" key="3" dd-mode="poster">
+                <input name="details-upload" type="file" accept=".pdf" dd-function="file-upload-input" key="3"
+                    dd-mode="poster">
                 <!-- END CONDITIONAL SECTION -->
                 <label class="labeled-checkbox transparent full">
-                    <input type="checkbox" name="intern-ausgrundung" <?= (get_field("project-intern-ausgrundung")==1) ? 'checked="checked"' : "" ?>>
-                    <span>Ich habe interesse an einer Ausgründung.</span>
+                    <input type="checkbox" name="intern-ausgrundung"
+                        <?= (get_field("project-intern-ausgrundung")==1) ? 'checked="checked"' : "" ?>>
+                    <span>Ich habe Interesse an einer Ausgründung.</span>
                 </label>
                 <a id="submit" class="bg-magenta color-white pl-2 pr-2">PROJEKT BEARBEITEN</a>
             </form>
@@ -168,15 +189,16 @@ if (!is_user_logged_in()) {
                             $img = get_template_directory_uri() . "/static/img/placeholder.jpg";
                         }
                         ?>
-                        <a class="pro-item labeled col" href="<?= the_permalink() ?>" dd-year="<?= $pro_years ?>" dd-type="<?= $pro_types ?>">
-                            <img src="<?= $img ?>"/>
-                            <div class="item-label">
-                                <div class="text-wrapper">
-                                    <p><?= get_field("project-details-name")  ?></p>
-                                </div>
-                            </div>
-                        </a>
-                        <?php
+                <a class="pro-item labeled col" href="<?= the_permalink() ?>" dd-year="<?= $pro_years ?>"
+                    dd-type="<?= $pro_types ?>">
+                    <img src="<?= $img ?>" />
+                    <div class="item-label">
+                        <div class="text-wrapper">
+                            <p><?= get_field("project-details-name")  ?></p>
+                        </div>
+                    </div>
+                </a>
+                <?php
                     }
                 }
                 ?>
@@ -190,36 +212,38 @@ if (!is_user_logged_in()) {
     wp_footer();
     ?>
     <script>
-        var mode = "default";
-        jQuery(document).ready(function($) {
-            // init
-            $("[dd-mode]").hide()
-            mode = $("#mode").val()
-            $(`[dd-mode=${mode}]:not(input[type=file])`).show()
-            // control events (switch between different html section)
-            $("#controls button").click(function() {
-                // toggle styling class
-                $("#controls").find("button").removeClass("active")
-                $(this).addClass("active")
-                // toggle relevant section
-                $(".conditional").hide()
-                const TARGET = $(this).attr("dd-target")
-                $(`#${TARGET}`).show()
-            })
-            // user events
-            $("#mode").change(function() {
-                mode = $(this).val()
-                $("[dd-mode]").hide()
-                $(`[dd-mode=${mode}]:not(input[type=file])`).show()
-            })
-            $("#submit").click(function(e) {
-                e.preventDefault(); //  prevents the <a> from acting like a link
-                if (mode != "default") {
-                    $(`[dd-mode]:not([dd-mode=${mode}])`).remove()
-                    $("#project-register-form")[0].reportValidity() && $("#project-register-form").trigger("submit")
-                }
-            })
+    var mode = "default";
+    jQuery(document).ready(function($) {
+        // init
+        $("[dd-mode]").hide()
+        mode = $("#mode").val()
+        $(`[dd-mode=${mode}]:not(input[type=file])`).show()
+        // control events (switch between different html section)
+        $("#controls button").click(function() {
+            // toggle styling class
+            $("#controls").find("button").removeClass("active")
+            $(this).addClass("active")
+            // toggle relevant section
+            $(".conditional").hide()
+            const TARGET = $(this).attr("dd-target")
+            $(`#${TARGET}`).show()
         })
+        // user events
+        $("#mode").change(function() {
+            mode = $(this).val()
+            $("[dd-mode]").hide()
+            $(`[dd-mode=${mode}]:not(input[type=file])`).show()
+        })
+        $("#submit").click(function(e) {
+            e.preventDefault(); //  prevents the <a> from acting like a link
+            if (mode != "default") {
+                $(`[dd-mode]:not([dd-mode=${mode}])`).remove()
+                $("#project-register-form")[0].reportValidity() && $("#project-register-form").trigger(
+                    "submit")
+            }
+        })
+    })
     </script>
 </body>
+
 </html>
