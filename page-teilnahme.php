1b1f680f0cab0428b6677137090570612abba5bc
Tim Herrmann <platinum2dollar@gmail.com>
2025-05-20 21:47:00 +0200

    Requested changes from Franzi

--- page-teilnahme.php
+++ page-teilnahme.php
@@ -27,65 +27,97 @@
                     </div>
                 </h2>
 
-                <div class="expand col gap-2" style="display:none">
+                <div class="expand col gap-1" style="display:none;">
 
                     <p>Wir danken allen Spendern und Sponsoren für ihre finanzielle Unterstützung. Ohne diese wären unsere Events in dieser Vielfalt nicht möglich.</p>
-
-                    <div class="col gap-1">
-                        <h4>Organisatorische Hinweise</h4>
-                        <p>
-                            Teilen Sie uns bitte die Namen des Standpersonals bis einer Woche vor Veranstaltung 
-                            für die Erstellung der Namensschilder und der WLAN-Logins mit. Der Standplan wird auf 
-                            dieser Seite für Sie bereitgestellt. Das Parken vor dem Haus ist nur für das Ab- bzw. 
-                            Beladen möglich. Bitte nutzen Sie die reservierte Parkfläche neben unserem Gebäude (3. Ebene).
-                        </p>
-                        <p>Bitte senden Sie uns für die Abbildung unserer Förderer das ausgefüllte Firmenprofil sowie ein druckfähiges Logo zu.</p>
-                        <a class="color-magenta" href="https://output-dd.de/wp-content/uploads/2018/04/Profil-OUTPUT-CONTACT_Fragebogen_blank.rtf">Firmenprofil herunterladen</a>
-                    </div>
-
                     
-                    <div class="col gap-1">
-                        <h4>Aufbau und Abbau</h4>
-                        <p><strong>Aufbau</strong> zwischen 12:00 und 13:00 Uhr</p>
-                        <p><strong>Abbau</strong> ab 18:30 Uhr am Ausstellungstag</p>
-                    </div>
+                    <div class="row gap-2">
+                        
+                        <div class="col gap-2" style="width: 60%">
+                            <div class="col gap-1">
+                                <h4>Organisatorische Hinweise</h4>
+                                <p>
+                                    Teilen Sie uns bitte die Namen des Standpersonals bis einer Woche vor Veranstaltung 
+                                    für die Erstellung der Namensschilder und der WLAN-Logins mit. Der Standplan wird auf 
+                                    dieser Seite für Sie bereitgestellt. Das Parken vor dem Haus ist nur für das Ab- bzw. 
+                                    Beladen möglich. Bitte nutzen Sie die reservierte Parkfläche neben unserem Gebäude (3. Ebene).
+                                </p>
+                                <p>Bitte senden Sie uns für die Abbildung unserer Förderer das ausgefüllte Firmenprofil sowie ein druckfähiges Logo zu.</p>
+                                <a class="color-magenta" href="https://output-dd.de/wp-content/uploads/2018/04/Profil-OUTPUT-CONTACT_Fragebogen_blank.rtf">Firmenprofil herunterladen</a>
+                            </div>
 
-                    <div class="col gap-1">
-                        <h4>Standplatz und Ausstattung</h4>
-                        <p>
-                            Größe von 2x3 m<br>
-                            1 Seminarraumtisch<br>
-                            2 Seminarraum-Stühle<br>
-                            Stromanschluss (Verteilerdose ist selbst mitzubringen)<br>
-                            WLAN-Login
-                        </p>
-                    </div>
+                            <div class="col gap-1">
+                                <h4>Aufbau und Abbau</h4>
+                                <p><strong>Aufbau</strong> zwischen 12:00 und 13:00 Uhr</p>
+                                <p><strong>Abbau</strong> ab 18:30 Uhr am Ausstellungstag</p>
+                            </div>
+                            
+                            <div class="col gap-1">
+                                <h4>Standplatz und Ausstattung</h4>
+                                <p>
+                                    Größe von 2x3 m<br>
+                                    1 Seminarraumtisch<br>
+                                    2 Seminarraum-Stühle<br>
+                                    Stromanschluss (Verteilerdose ist selbst mitzubringen)<br>
+                                    WLAN-Login
+                                </p>
+                            </div>
+                        </div>
 
-                    <div class="col gap-1">
-                        <h4>Programm</h4>
-                        <p>
-                            Um einen Vortrag für OUTPUT einzureichen, muss dieser als Projekt <a class="color-magenta" href="/projekt-einreichen">hier angemeldet</a> werden. 
-                            Mit dieser Anmeldung wird der Vortrag sowohl auf der Webseite als auch in der OUTPUT-App angezeigt.
-                        </p>
-                    </div>
+                        <div class="col gap-2" style="width:40%">
+                            <div class="col gap-1">
+                                <h4>Programm</h4>
+                                <p>
+                                    Um einen Vortrag für OUTPUT einzureichen, muss dieser als Projekt <a class="color-magenta" href="/projekt-einreichen">hier angemeldet</a> werden. 
+                                    Mit dieser Anmeldung wird der Vortrag sowohl auf der Webseite als auch in der OUTPUT-App angezeigt.
+                                </p>
+                            </div>
+                            
+                            <div class="col gap-1">
+                                <h4>Versorgung</h4>
+                                <p>
+                                    Unser Studentencafé Ascii bietet Ihnen während der Veranstaltung Kaffee, Tee und kalte Getränke an. 
+                                    Zudem haben wir Automaten für Getränke und Snacks.
+                                </p>
+                                <p>Ab 18:00 Uhr gibt es Gegrilltes und Getränke in der OUTPUT Lounge.</p>
+                            </div>
+                        </div>
                     
-                    <div class="col gap-1">
-                        <h4>Versorgung</h4>
-                        <p>
-                            Unser Studentencafé Ascii bietet Ihnen während der Veranstaltung Kaffee, Tee und kalte Getränke an. 
-                            Zudem haben wir Automaten für Getränke und Snacks.
-                        </p>
-                        <p>Ab 18:00 Uhr gibt es Gegrilltes und Getränke in der OUTPUT Lounge.</p>
                     </div>
                 
                 </div>
 
             </div>
 
+            <h2>OUTPUT APP</h2>
+            <div class="grid">
+                <div class="c1 col gap-2">
+                    <p>
+                        Die App bietet Informationen zur Projektschau OUTPUT.DD. Einmal im Jahr werden in diesem Rahmen die Forschungsergebnisse von Studenten 
+                        und Mitarbeitern der breiten Öffentlichkeit präsentiert. Die Besucher erwarten Installationen, Workshops, Vorträge, Präsentationen und 
+                        Ausstellungen zum Entdecken, Erleben und Erforschen. Daneben bietet OUTPUT.DD die Möglichkeit, mit ansässigen Firmen über spannende 
+                        Forschungsfragen ins Gespräch zu kommen. Die App bietet einen Überblick über alle Veranstaltungen und Projekte und verknüpft diese mit 
+                        den jeweiligen Veranstaltungsorten im Fakultätsgebäude.
+                    </p>
+                    <p>
+                        Die Besucher können im Rahmen des integrierten Spiels bei allen Projekten und Firmen Punkte sammeln und sich damit in einer Rangliste 
+                        platzieren. 
+                        <br>
+                        <b>Den ersten fünf Plätzen winken Preise im Wert von insgesamt 500 EURO.</b>
+                    </p>
+                </div>
+                <div>
+                    <img class="c2 app-img" src="https://output-dd.de/wp-content/uploads/2025/05/app_1.png">
+                </div>
+                <div>
+                    <img class="c3 app-img" src="https://output-dd.de/wp-content/uploads/2025/05/app_2.png">
+                </div>
+            </div>
+
             <div class="col gap-2" dd-function="expandable">
                 
                 <h2 dd-function="trigger">
-                    Teilnahme als Besucher
+                    OUTPUT APP als Besucher
                     <div class="icon ml-1">
                         <span class="iconify" data-icon="mdi-chevron-down">
                     </div>
@@ -121,7 +153,7 @@
             <div class="col gap-2" dd-function="expandable">
                 
                 <h2 dd-function="trigger">
-                    Teilnahme als Aussteller
+                    OUTPUT APP als Aussteller
                     <div class="icon ml-1">
                         <span class="iconify" data-icon="mdi-chevron-down">
                     </div>
@@ -156,31 +188,6 @@
 
                 </div>
 
-            </div>
-            
-            <h2>OUTPUT APP</h2>
-            <div class="grid">
-                <div class="c1 col gap-2">
-                    <p>
-                        Die App bietet Informationen zur Projektschau OUTPUT.DD. Einmal im Jahr werden in diesem Rahmen die Forschungsergebnisse von Studenten 
-                        und Mitarbeitern der breiten Öffentlichkeit präsentiert. Die Besucher erwarten Installationen, Workshops, Vorträge, Präsentationen und 
-                        Ausstellungen zum Entdecken, Erleben und Erforschen. Daneben bietet OUTPUT.DD die Möglichkeit, mit ansässigen Firmen über spannende 
-                        Forschungsfragen ins Gespräch zu kommen. Die App bietet einen Überblick über alle Veranstaltungen und Projekte und verknüpft diese mit 
-                        den jeweiligen Veranstaltungsorten im Fakultätsgebäude.
-                    </p>
-                    <p>
-                        Die Besucher können im Rahmen des integrierten Spiels bei allen Projekten und Firmen Punkte sammeln und sich damit in einer Rangliste 
-                        platzieren. 
-                        <br>
-                        <b>Den ersten fünf Plätzen winken Preise im Wert von insgesamt 500 EURO.</b>
-                    </p>
-                </div>
-                <div>
-                    <img class="c2 app-img" src="https://output-dd.de/wp-content/uploads/2025/05/app_1.png">
-                </div>
-                <div>
-                    <img class="c3 app-img" src="https://output-dd.de/wp-content/uploads/2025/05/app_2.png">
-                </div>
             </div>
 
         </div>
