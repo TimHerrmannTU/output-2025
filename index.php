4f302044296ca5fa6f05b21ce20eabb965397393
Charles Krüger <charles.krueger.social@gmail.com>
2025-05-20 18:10:37 +0200

    fix date on home page

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
     <div class="head">
-        <?php 
+        <?php
         include get_template_directory() . "/includes/tud-navbar.php";
-        include get_template_directory() . "/includes/navbar.php"; 
-        include get_template_directory() . "/includes/mobile-navbar.php"; 
+        include get_template_directory() . "/includes/navbar.php";
+        include get_template_directory() . "/includes/mobile-navbar.php";
         ?>
 
         <div id="banner" class="full">
@@ -19,8 +21,9 @@
                 <div class="text-content col">
                     <h2>DIE PROJEKTSCHAU DER INFORMATIK</h2>
                     <h2>19. JUNI 2025</h2>
-                    <h3>WERDE TEIL VON OUTPUT UND<br> REICHE DEIN PROJEKT BIS 5. JUNI EIN!</h3>
-                    <button class="white transparent-text mt-3" onclick="location.href='/projekt-einreichen'">PROJEKT EINREICHEN</button>
+                    <h3>WERDE TEIL VON OUTPUT UND<br> REICHE DEIN PROJEKT BIS 6. JUNI EIN!</h3>
+                    <button class="white transparent-text mt-3" onclick="location.href='/projekt-einreichen'">PROJEKT
+                        EINREICHEN</button>
                 </div>
             </div>
         </div>
@@ -33,9 +36,11 @@
             <div class="text-wrapper dark-bg col gap-2">
                 <h3 class="fat color-magenta">DIE PROJEKTSCHAU DER FAKULTÄT INFORMATIK AN DER TU DRESDEN</h3>
                 <p>
-                    Einmal im Jahr präsentieren Studenten und Mitarbeiter ihre Ergebnisse aus Lehre und Forschung der Öffentlichkeit. 
-                    Dich erwarten spannende Installationen, Workshops, Vorträge und Ausstellungen. 
-                    Zudem bietet OUTPUT.DD die Gelegenheit, mit Unternehmen über aktuelle Forschungsfragen ins Gespräch zu kommen.
+                    Einmal im Jahr präsentieren Studenten und Mitarbeiter ihre Ergebnisse aus Lehre und Forschung der
+                    Öffentlichkeit.
+                    Dich erwarten spannende Installationen, Workshops, Vorträge und Ausstellungen.
+                    Zudem bietet OUTPUT.DD die Gelegenheit, mit Unternehmen über aktuelle Forschungsfragen ins Gespräch
+                    zu kommen.
                 </p>
                 <p class="fat">
                     Die Veranstaltung ist offen für alle.<br>Wir freuen uns auf dich!
@@ -44,7 +49,7 @@
         </div>
     </div>
 
-    <?php 
+    <?php
     include get_template_directory() ."/includes/challenges.php";
     include get_template_directory() ."/includes/carousels/projekte.php";
     include get_template_directory() ."/includes/carousels/sponsoren.php";
@@ -52,4 +57,5 @@
     wp_footer();
     ?>
 </body>
+
 </html>
